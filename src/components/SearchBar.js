91887a1aa0218d54141543f29bab6d490d913579
john.grindall@gmail.com <john.grindall@gmail.com>
2017-05-06 17:13:27 +0100

    state added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,11 +6,20 @@ const SearchBarFun = () => {
 };
 
 class SearchBar extends React.Component{
-    render(){
-        return <input onChange = {this.onTextChange}/>;
+    constructor(props){
+        super(props);
+        this.state = {"term":"type here"};
     }
-    onTextChange(){
-        console.log(arguments);
+    render(){
+        return <div>
+            <input onChange = {this.onTextChange.bind(this)}/>
+            <span>{this.state.term}</span>    
+        </div>;
+    }
+    onTextChange(e){
+        var val = e.target.value;
+        console.log(e, val);
+        this.setState({"term": val});
     }
 }
 
